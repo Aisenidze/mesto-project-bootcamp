a178fa60cc81a0098d5ed614ff25322380aeed08
Aisen Ivanov <Aisenidze@gmail.com>
2023-02-21 22:07:35 +0700

    3 коммит

---
+++ index.js
@@ -0,0 +1,10 @@
+const profileEditButton = querySelector('.profile__editor');
+
+function openPopUp() {
+    const popup = document.querySelector('.popup');
+    popup.classList.add('popup_visible');
+}
+
+buttonInfo.addEventListener('click', openPopUp);
+
+openPopUp;
